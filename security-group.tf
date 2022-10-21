9175dc1a614b233a74f0d01553592ada7c0003b8
mauroslucios <mauroslucios@gmail.com>
2022-10-20 22:10:17 -0300

    Insert vars.tf

--- security-group.tf
+++ security-group.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "acesso-ssh"{
         from_port = 22
         to_port   = 22
         protocol  = "tcp"
-        cidr_blocks = ["138.117.221.194/32"]
+        cidr_blocks = var.cdirs_acesso_remoto
     }
 
     tags = {
@@ -24,7 +24,7 @@ resource "aws_security_group" "acesso-ssh-us-east-2"{
         from_port = 22
         to_port   = 22
         protocol  = "tcp"
-        cidr_blocks = ["138.117.221.194/32"]
+        cidr_blocks = var.cdirs_acesso_remoto
     }
 
     tags = {
