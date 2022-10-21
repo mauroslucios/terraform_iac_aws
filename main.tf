e87369c1ac8311a74a5484ebb9289708298e42c7
mauroslucios <mauroslucios@gmail.com>
2022-10-20 22:09:41 -0300

    Insert vars.tf

--- main.tf
+++ main.tf
@@ -22,50 +22,50 @@ resource "aws_instance" "dev"{
     count = 3
     ami = var.ami["us-east-1"]
     instance_type = "t2.micro"
-    key_name = "terraform-aws"
+    key_name = var.key_name
     tags = {
         Name = "dev${count.index}"
     }
     vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
 
+resource "aws_instance" "dev3"{
+    ami = var.ami["us-east-1"]
+    instance_type = "t2.micro"
+    key_name = var.key_name
+    tags = {
+        Name = "dev3"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+    depends_on = [aws_s3_bucket.dev1]
+}
+
 resource "aws_instance" "dev4"{
     ami = var.ami["us-east-1"]
     instance_type = "t2.micro"
-    key_name = "terraform-aws"
+    key_name = var.key_name
     tags = {
         Name = "dev4"
     }
     vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
-    depends_on = [aws_s3_bucket.dev4]
 }
 
 resource "aws_instance" "dev5"{
-    ami = var.ami["us-east-1"]
-    instance_type = "t2.micro"
-    key_name = "terraform-aws"
-    tags = {
-        Name = "dev5"
-    }
-    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
-}
-
-resource "aws_instance" "dev6"{
     ami = var.ami["us-east-2"]
     provider = aws.us-east-2
     instance_type = "t2.micro"
-    key_name = "terraform-aws"
+    key_name = var.key_name
     tags = {
-        Name = "dev6"
+        Name = "dev5"
     }
     vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
     depends_on = [aws_dynamodb_table.dynamodb-dev]
 }
 
-resource "aws_s3_bucket" "dev4"{
-    bucket = "repobucket-dev4"
+resource "aws_s3_bucket" "dev1"{
+    bucket = "repobucket-dev1"
     #acl    = "private"
     tags = {
-        Name = "repobucket-dev4"
+        Name = "repobucket-dev1"
     }
 }
