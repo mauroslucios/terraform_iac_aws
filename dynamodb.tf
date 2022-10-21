39a29251a7dc68dccd83c7ebb623a29e81753e10
mauroslucios <mauroslucios@gmail.com>
2022-10-20 21:18:55 -0300

    Updated dynamoDb

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "dynamodb-dev" {
-  provider = "aws.us-east-2"
+  provider = aws.us-east-2
   name           = "GameScores"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "UserId"
